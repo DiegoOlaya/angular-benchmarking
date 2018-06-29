65b25ebfb5b875890db09be5a12118503ec21cb9
Diego Olaya <dolaya@caltech.edu>
2018-06-29 15:35:01 -0600

    Create class defining imaginary number object.

---
+++ src/app/object-tester/imaginary-num.ts
@@ -0,0 +1,6 @@
+export class ImaginaryNum {
+    constructor( //Sets the two state variables for the imaginary number object.
+        public real: number,
+        public imaginary: number
+    ) {}
+}
