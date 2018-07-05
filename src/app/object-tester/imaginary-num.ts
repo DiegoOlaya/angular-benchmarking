969b70c5f59751b00809607d4a8a948c85c15fcd
Diego Olaya <dolaya@caltech.edu>
2018-07-05 17:51:27 -0500

    Imaginary-num constructor now sets values of real and imaginary portions

--- src/app/object-tester/imaginary-num.ts
+++ src/app/object-tester/imaginary-num.ts
@@ -1,6 +1,11 @@
 export class ImaginaryNum {
-    constructor( //Sets the two state variables for the imaginary number object.
-        public real: number,
-        public imaginary: number
-    ) {}
+  public real: number,
+  public imaginary: number
+
+  constructor( //Sets the two state variables for the imaginary number object.
+    randRange: number //Argument determines the range of numbers possible.
+  ) {
+    this.real = Math.random()*randRange;
+    this.imaginary = Math.random()*randRange;
+  }
 }
