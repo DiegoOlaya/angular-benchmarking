2746f438c55ce4357b7e47085480ba718722df84
Diego Olaya <dolaya@caltech.edu>
2018-07-20 16:44:25 -0600

    Wrote backend to average multiple runs for all implementations.

--- src/app/timing-averager/timing-averager.component.ts
+++ src/app/timing-averager/timing-averager.component.ts
@@ -1,11 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ArrayTesterComponent } from './array-tester';
+import { ObjectTesterComponent } from './object-tester';
+import { NumberTesterComponent } from '.number-tester';
+
 @Component({
   selector: 'app-timing-averager',
   templateUrl: './timing-averager.component.html',
   styleUrls: ['./timing-averager.component.css']
 })
 export class TimingAveragerComponent implements OnInit {
+
+  private timingComponent:Object;
+  private componentArray = [
+    new ObjectTesterComponent(), new ArrayTesterComponent(), new NumberTesterComponent()
+  ];
+
+  public averagedTime:number;
+
+  public setTimingComponent(index:number) {
+    this.timingComponent = this.componentArray[index];
+  }
+
+  public startTiming(numRuns:number, cyclesPerRun:number, range:number) {
+    var timer = window.performance;
+    var totalTime:number = 0;
+    for (var i = 0; i < numRuns; i++) {
+      var start = timer.now();
+      this.timingComponent.startTiming(cyclesPerRun, range);
+      var end = timer.now()
+      totalTime += (end-start);
+    }
+    this.averagedTime = (totalTime/numRuns);
+  }
 
   constructor() { }
 
